ebf65473c0963e2693272b58f0283e6403ebac3e
Yuvem <matjuve33@icloud.com>
2025-11-06 15:30:42 +0100

    auth front page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Manrope } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+// Chargement des polices Google
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
+  display: "swap",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,9 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${manrope.variable} font-sans antialiased`}
       >
         {children}
       </body>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div className="">
+      <div className="bg-[#F9FAFB] w-[35%] h-screen flex flex-col items-center justify-between py-20">
+        <Image alt="" height={100} width={250} src="/Logo.png" />
+
+        <div className="w-full flex flex-col items-center gap-10">
+          <h1 className="text-[#D3590B] font-bold text-4xl">Connexion</h1>
+          <form action="" className="flex flex-col w-[50%] gap-5">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="mail" className="text-sm">Email</label>
+              <input type="email" id="mail" className="bg-white border border-[#E5E7EB] h-12 px-2" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="password" className="text-sm">Mot de passe</label>
+              <input type="password" id="password" className="bg-white border border-[#E5E7EB] h-12 px-2" />
+            </div>
+
+            <button className="bg-[#1F1F1F] text-lg text-white rounded-xl h-12 w-[85%] self-center hover:opacity-80">Se connecter</button>
+            <u className="text-[#D3590B] text-sm self-center">Mot de passe oublié?</u>
+          </form>
+        </div>
+
+        <p className="text-sm">
+          Pas encore de compte ? <u className="text-[#D3590B]"><Link href="/sign-in">Créer un compte</Link></u>
+        </p>
+      </div>
+
+      <Image alt="" fill className="-z-10 object-cover" src="/login.jpg" />
+    </div>
+  );
 }
